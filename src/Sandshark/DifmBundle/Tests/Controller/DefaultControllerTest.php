3b3d7fa4bed4a2c99d1cae1dcf2468ffd99a4f83
vernija <jan.vernieuwe@phpro.be>
2015-05-17 21:15:51 +0200

    Updates tests

--- src/Sandshark/DifmBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Sandshark/DifmBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,7 +10,7 @@ class DefaultControllerWebTest extends DifmWebTestCase
     public function testPremiumPls()
     {
         $client = static::createClient();
-        $client->request('GET', '/premium/test.pls');
+        $client->request('GET', '/difm/premium/test.pls');
         $this->assertContains('[playlist]', $client->getResponse()->getContent());
         $this->assertContains('NumberOfEntries', $client->getResponse()->getContent());
         $this->assertContains('File1', $client->getResponse()->getContent());
@@ -23,7 +23,7 @@ class DefaultControllerWebTest extends DifmWebTestCase
     public function testPublicPls()
     {
         $client = static::createClient();
-        $client->request('GET', '/public/test.pls');
+        $client->request('GET', '/difm/public/test.pls');
         $this->assertContains('[playlist]', $client->getResponse()->getContent());
         $this->assertContains('NumberOfEntries', $client->getResponse()->getContent());
         $this->assertContains('File1', $client->getResponse()->getContent());
@@ -36,7 +36,7 @@ class DefaultControllerWebTest extends DifmWebTestCase
     public function testPremiumM3u()
     {
         $client = static::createClient();
-        $client->request('GET', '/premium/test.m3u');
+        $client->request('GET', '/difm/premium/test.m3u');
         $this->assertContains('#EXTM3U', $client->getResponse()->getContent());
         $this->assertContains('#EXTINF:-1,', $client->getResponse()->getContent());
         $this->assertContains('http://prem', $client->getResponse()->getContent());
@@ -46,7 +46,7 @@ class DefaultControllerWebTest extends DifmWebTestCase
     public function testPublicM3u()
     {
         $client = static::createClient();
-        $client->request('GET', '/public/test.m3u');
+        $client->request('GET', '/difm/public/test.m3u');
         $this->assertContains('#EXTM3U', $client->getResponse()->getContent());
         $this->assertContains('#EXTINF:-1,', $client->getResponse()->getContent());
         $this->assertContains('http://pub', $client->getResponse()->getContent());
@@ -56,7 +56,7 @@ class DefaultControllerWebTest extends DifmWebTestCase
     public function testInvalidFormat()
     {
         $client = static::createClient();
-        $client->request('GET', '/premium/test.invalid');
+        $client->request('GET', '/difm/premium/test.invalid');
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 }
