01c7c8edd5e4278c564d8ac93b8a8bd6183a833c
vernija <jan.vernieuwe@phpro.be>
2015-05-17 03:45:29 +0200

    Added Tests for free playlists

--- src/Sandshark/DifmBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Sandshark/DifmBundle/Tests/Controller/DefaultControllerTest.php
@@ -7,7 +7,7 @@ use Sandshark\DifmBundle\Tests\DifmWebTestCase;
 class DefaultControllerWebTest extends DifmWebTestCase
 {
 
-    public function testPls()
+    public function testPremiumPls()
     {
         $client = static::createClient();
         $client->request('GET', '/premium/test.pls');
@@ -16,16 +16,40 @@ class DefaultControllerWebTest extends DifmWebTestCase
         $this->assertContains('File1', $client->getResponse()->getContent());
         $this->assertContains('Title1', $client->getResponse()->getContent());
         $this->assertContains('Length1', $client->getResponse()->getContent());
+        $this->assertContains('http://prem', $client->getResponse()->getContent());
         $this->assertContains('?test', $client->getResponse()->getContent());
     }
 
-    public function testM3u()
+    public function testPublicPls()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/public/test.pls');
+        $this->assertContains('[playlist]', $client->getResponse()->getContent());
+        $this->assertContains('NumberOfEntries', $client->getResponse()->getContent());
+        $this->assertContains('File1', $client->getResponse()->getContent());
+        $this->assertContains('Title1', $client->getResponse()->getContent());
+        $this->assertContains('Length1', $client->getResponse()->getContent());
+        $this->assertContains('http://pub', $client->getResponse()->getContent());
+        $this->assertContains('?test', $client->getResponse()->getContent());
+    }
+
+    public function testPremiumM3u()
     {
         $client = static::createClient();
         $client->request('GET', '/premium/test.m3u');
         $this->assertContains('#EXTM3U', $client->getResponse()->getContent());
         $this->assertContains('#EXTINF:-1,', $client->getResponse()->getContent());
-        $this->assertContains('http://prem2.di.fm:80', $client->getResponse()->getContent());
+        $this->assertContains('http://prem', $client->getResponse()->getContent());
+        $this->assertContains('?test', $client->getResponse()->getContent());
+    }
+
+    public function testPublicM3u()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/public/test.m3u');
+        $this->assertContains('#EXTM3U', $client->getResponse()->getContent());
+        $this->assertContains('#EXTINF:-1,', $client->getResponse()->getContent());
+        $this->assertContains('http://pub', $client->getResponse()->getContent());
         $this->assertContains('?test', $client->getResponse()->getContent());
     }
 
