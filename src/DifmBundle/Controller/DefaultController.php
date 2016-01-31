1d5fd6a36dbac6c98fdc864f1a922e687a31443f
vernija <jan.vernieuwe@phpro.be>
2016-02-01 00:11:54 +0100

    Moved parameter validation to routing.yml

--- src/DifmBundle/Controller/DefaultController.php
+++ src/DifmBundle/Controller/DefaultController.php
@@ -6,7 +6,6 @@ use DifmBundle\Api\Channels;
 use DifmBundle\Playlist\M3u;
 use DifmBundle\Playlist\Pls;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -17,9 +16,6 @@ class DefaultController extends Controller
 
     public function downloadAction($station, $key, $_format)
     {
-        if (!in_array($station, ['difm', 'rockradio', 'jazzradio', 'radiotunes'])) {
-            throw new NotFoundHttpException(sprintf('Station %s not supported', $station));
-        }
         /** @var Channels $channels */
         $channels = $this->get($station . '.channels');
         $playlist = $_format === 'm3u' ? new M3u($channels) : new Pls($channels);
